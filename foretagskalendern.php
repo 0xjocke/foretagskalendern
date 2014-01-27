6b8a7d00355de909751576583679b065eb7b0df3
Joachim <joachim.bachstatter@gmail.com>
2014-01-27 13:54:38 +0100

    Sådärja

--- foretagskalendern.php
+++ foretagskalendern.php
@@ -3,7 +3,7 @@
  * Plugin Name: Företagskalendern
  * Description: Håll koll på alla viktiga datum
  * Version: 0.0.1
- * Author: Joachim, Nakhleh
+ * Author: Joachim, Nakhlé
  * Author URI: http://bachstatter.se 
  * License: Fortnox hejsan svejsan
  */
