07b8aae59cd4d7434199dc8a79a8128bf387aac7
nakfra <NellieStefanie2012>
2014-01-27 15:48:58 +0100

    Har  skapat en funktion och lägga till ett filter som lägger till en kodsnutt (tex en adsensekod eller något annat) i slutet av varje inläsi

--- foretagskalendern.php
+++ foretagskalendern.php
@@ -1,10 +1,35 @@
+
+
 <?php
 /**
  * Plugin Name: Företagskalendern
  *Plugin URI: http://www.fortnox.se
  * Description: Håll koll på alla viktiga datum
  * Version: 0.0.1
- * Author: Joachim, Nakhlé
+ * Author: Joachim, Nakleh
  * Author URI: http://bachstatter.se 
  * License: Fortnox hejsan svejsan
  */
+
+
+/*
+function mitt_lilla_plugin_add_code ( $content ) {  //Funktionen tar emot ett värde nämligen $content. Dvs innehållet i inlägget.
+
+    // Vi vill bara lägga till koden på enskilda inlägg
+    if ( is_single() ) { 
+
+        //lägger till koden i slutet av inlägget
+        $content .= 'Klistra in din kod eller något annat innehåll'; // Vi lägger till koden genom .= för att inte skriva över allt innehåll.
+
+    }
+
+    // returnera det nya innehållet
+    return $content;
+}
+
+//lägga till ett filter som aktiverar vår filtreringsfunktion
+add_filter( 'the_content', 'mitt_lilla_plugin_add_code' );
+
+*/
+?>
+
