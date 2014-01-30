8005d9a1d8dd8715c3a5c1dfceb5475b2ebe671b
Joachim <joachim.bachstatter@gmail.com>
2014-01-30 10:21:02 +0100

    sending form value with ajax

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -2,50 +2,38 @@
 	"use strict";
 
 	$(function () {
-		var Foretagskalender = function(values){
-			var name = "";
-			var momsperiod = "";
-			var timeReport = "";
-			this.construct = function(values){
-				if (values == null) {
-					return;
-				}else{
-					this.name = values[0];
-					this.momsperiod = values[1];
-					this.timeReport = values[2];
-				}
-			}
-			this.ajaxsender = function(){
-				var pluginPath = fk_plugin_url.directory;
-				$.ajax({
-					url: pluginPath + '/foretagskalendern/public/php/makepdf.php',
-					type: 'POST',
-					data: {'name': this.name, 'momsperiod':this.momsperiod, 'timeReport': this.timeReport},
-				})
-				.done(function() {
-					console.log("success");
-					$('#fk-downloadlink').css('display', 'block');
-				})
-				.fail(function() {
-					console.log("error");
-				})
-				.always(function() {
-					console.log("complete");
-				});
-				
-			}
-
-			this.construct(values);
+		 function Foretagskalender(values){
+			this.name = values[0].value;
+			this.momsperiod = values[1].value;
+			this.timeReport = values[2].value;
+		
+		}
+	
+		Foretagskalender.prototype.ajaxsender = function(){
+			var pluginPath = fk_plugin_url.directory;
+			$.ajax({
+				url: pluginPath + '/foretagskalendern/public/php/makepdf.php',
+				type: 'POST',
+				data: {'name': this.name, 'momsperiod':this.momsperiod, 'timeReport': this.timeReport},
+			})
+			.done(function() {
+				console.log("success");
+				$('#fk-downloadlink').css('display', 'block');
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});	
 		}
 
-		$('#fk-form').submit(function(e){ e.preventDefault(); });
-	
-		$(document).on('click', '#fk-btn', function() {
-			var values = $('#fk-form').serializeArray();
-			var foretagskalender1 = new Foretagskalender(values);
+		$(document).on('submit', '#fk-form', function(e) {
+			e.preventDefault();
+			var values = $(this).serializeArray();
+ 			var foretagskalender1 = new Foretagskalender(values);
 			foretagskalender1.ajaxsender();
 
-        
 		});
 	});
 
