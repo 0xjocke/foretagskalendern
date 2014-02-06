e815f15cce80199bb5faffe4b1cecf54ce68c11d
Joachim <joachim.bachstatter@gmail.com>
2014-02-06 16:41:40 +0100

    eu, paper, fiscalyear end and start added

--- public/js/public.js
+++ public/js/public.js
@@ -2,14 +2,16 @@
 	"use strict";
 
 	$(function () {
-		 function Foretagskalender(values){
+		function Foretagskalender(values){
 			this.name = values[0].value;
 			this.momsperiod = values[1].value;
 			this.timeReport = values[2].value;
-			this.financialStatement = values[3].value;
-			this.extra = values[4].value;
-			this.extraDate = values[5].value;
-		
+			this.fiscalYearStart = values[3].value;
+			this.fiscalYearEnd = values[4].value;
+			this.eu = values[5].value;
+			this.paper = values[6].value;
+			this.extra = values[7].value;
+			this.extraDate = values[8].value;
 		}
 	
 		Foretagskalender.prototype.ajaxsender = function(path){
@@ -17,14 +19,17 @@
 			$.ajax({
 				url: pluginPath + path,
 				type: 'POST',
-				data: {'name': this.name, 'momsperiod':this.momsperiod, 'financialStatement' : this.financialStatement, 'timeReport': this.timeReport, 'extra' : this.extra, 'extraDate': this.extraDate},
+				data: {'name': this.name, 'momsperiod':this.momsperiod,
+				'fiscalYearStart' : this.fiscalYearStart, 'fiscalYearEnd' : this.fiscalYearEnd,
+				'eu' : this.eu, 'paper' : this.paper, 'timeReport': this.timeReport,
+				'extra' : this.extra,'extraDate': this.extraDate},
 			})
 			.done(function(guid) {
 				console.log("success");
 				if (path === '/foretagskalendern/public/php/makepdf.php' ) {
-					$('#fk-pdflink').css('display', 'inline-block').attr('href', pluginPath + '/foretagskalendern/public/php/makepdf.php?guid=' + guid);
+					$('#fk-pdflink').fadeIn('fast').attr('href', pluginPath + '/foretagskalendern/public/php/makepdf.php?guid=' + guid);
 				}else{
-					$('#fk-icallink').css('display', 'inline-block').attr('href', pluginPath + '/foretagskalendern/public/views/ical.ics');
+					$('#fk-icallink').fadeIn('fast').attr('href', pluginPath + '/foretagskalendern/public/views/ical.ics');
 				}
 			})
 			.fail(function() {
@@ -32,13 +37,13 @@
 			})
 			.always(function() {
 				console.log("complete");
-			});	
-		}
+			});
+		};
 
 		$(document).on('submit', '#fk-form', function(e) {
 			e.preventDefault();
 			var values = $(this).serializeArray();
- 			var foretagskalender1 = new Foretagskalender(values);
+			var foretagskalender1 = new Foretagskalender(values);
 			foretagskalender1.ajaxsender('/foretagskalendern/public/php/makepdf.php');
 			foretagskalender1.ajaxsender('/foretagskalendern/public/php/makeical.php');
 
